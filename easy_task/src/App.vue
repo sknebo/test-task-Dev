8adb40ed9e692d10998a8d052ab364fc554e0c94
Alexander Vovnyanko <vovnyankoaleksander@gmail.com>
2021-03-15 19:23:21 +0300

    working maps wuth marker point and getting coordinations

--- easy_task/src/App.vue
+++ easy_task/src/App.vue
@@ -1,26 +1,48 @@
 <template>
   <div id="app">
+    <yandex-map
+      :settings="settings"
+      :coords="coords"
+      :zoom="10"
+      @click="onClick"
+    >
+      <ymap-marker
+        :coords="coords"
+        marker-id="123"
+        hint-content="some hint"
+        :balloon-template="balloonTemplate"
+      />
+    </yandex-map>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    coords: [55.751173, 37.622423],
+    addr: "Здесь адрес доставки",
+  }),
+  computed: {
+    balloonTemplate() {
+      return `<p>Вы кликнули сюда: ${this.coords}</p>`;
+    },
+  },
+  methods: {
+    onClick(e) {
+      this.coords = e.get("coords");
+    },
+  },
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+}
+
+.ymap-container {
+  height: 100%;
 }
 </style>
